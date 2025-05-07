fe380d3b3c7b141d8e1e734ea507091a48290193
EmilienHed <emilien.hedin@viacesi.fr>
2025-05-08 00:27:22 +0200

    Mise à jour des chemins dans le module d'articles

--- frontend/src/app/modules/articles/articles.module.ts
+++ frontend/src/app/modules/articles/articles.module.ts
@@ -3,17 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { ArticleListComponent } from './components/article-list/article-list.component';
-import { ArticleDetailComponent } from './components/article-detail/article-detail.component';
-import { ArticleFormComponent } from './components/article-form/article-form.component';
 import { AdminGuard } from '../../guards/admin.guard';
+import {ArticleListComponent} from '../../components/article-list/article-list.component';
+import {ArticleDetailComponent} from '../../components/article-detail/article-detail.component';
+import {ArticleFormComponent} from '../../components/article-form/article-form.component';
 
 // Définissez les routes du module
 const routes: Routes = [
   { path: '', component: ArticleListComponent },
   { path: ':id', component: ArticleDetailComponent },
-  { path: 'admin/create', component: ArticleFormComponent, canActivate: [AdminGuard] },
-  { path: 'admin/edit/:id', component: ArticleFormComponent, canActivate: [AdminGuard] }
+  { path: 'create', component: ArticleFormComponent, canActivate: [AdminGuard] },
+  { path: 'edit/:id', component: ArticleFormComponent, canActivate: [AdminGuard] }
 ];
 
 @NgModule({
