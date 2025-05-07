228f0855390f807889cd8fb49b9a378a6b173925
EmilienHed <emilien.hedin@viacesi.fr>
2025-05-08 00:17:12 +0200

    Modification du routage pour utiliser le module d'articles

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,22 +1,37 @@
-/*
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
-
-*/
-
-// src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 import { AuthGuard } from './guards/auth.guard';
+import { RespirationPracticeComponent } from './components/Respiration/respiration-practice/respiration-practice.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+// Exporter les routes pour qu'elles soient utilisables dans d'autres fichiers
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  // rediriger vers la page d'accueil pour toute route inconnue
-  { path: '**', redirectTo: '' }
+  { path: 'inscription', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
+  {
+    path: 'respiration/practice/:id',
+    component: RespirationPracticeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'respiration',
+    loadChildren: () => import('./modules/respiration/respiration.module').then(m => m.RespirationModule),
+    canActivate: [AuthGuard]
+  },
+  // Routes pour les articles - utilisation du module d'articles
+  {
+    path: 'articles',
+    loadChildren: () => import('./modules/articles/articles.module').then(m => m.ArticlesModule),
+    canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
