98d46cab18ccb73dfed39d241647074ecfc649f7
EmilienHed <emilien.hedin@viacesi.fr>
2025-06-13 14:49:03 +0200

    prerender change

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,6 +2,23 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'respiration/practice/:id',
+    renderMode: RenderMode.Server  // Désactive le prerender statique ici
+  },
+  {
+    path: 'articles/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin/articles/edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin/users/edit/:id',
+    renderMode: RenderMode.Server
+  },
+  // Pour toutes les autres routes sans paramètre dynamique, tu peux garder Prerender
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
